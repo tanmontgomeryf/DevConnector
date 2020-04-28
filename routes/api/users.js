9845f5bb8bedca4a2c30f76be6bde73d56683fbd
Monty Tan <tanmontgomeryf@gmail.com>
2020-04-28 19:04:59 +0800

    added jsonwebtoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,9 @@ const express = require('express');
 const gravatar = require('gravatar');
 const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
-var bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 
@@ -62,8 +64,21 @@ router.post(
       await user.save();
 
       //return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
-      res.send('User registered');
+      jwt.sign(
+        payload,
+        config.get('jwtToken'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
